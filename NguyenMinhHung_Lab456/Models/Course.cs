3adaa22c5237fbc1942624b6677711f8ef1f87af
Administrator <Administrator@PC243>
2020-06-05 16:20:58 +0700

    Hung them mot dau cach

--- NguyenMinhHung_Lab456/Models/Course.cs
+++ NguyenMinhHung_Lab456/Models/Course.cs
@@ -18,6 +18,6 @@ namespace NguyenMinhHung_Lab456.Models
         public DateTime DateTime { get; set; }
         public Category Category { get; set; }
         [Required]
-        public byte CategoryId { get; set; }
+        public byte CategoryId { get; set; } 
     }
 }
